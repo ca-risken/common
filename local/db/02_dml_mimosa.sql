5ca12c1cdd83b23e07e3eab57e5a49c9e2668723
gassara-kys <ogasawara_kiyoshi@cyberagent.co.jp>
2020-06-09 16:29:18 +0900

    fix dml

--- local/db/02_dml_mimosa.sql
+++ local/db/02_dml_mimosa.sql
@@ -3,7 +3,7 @@ set character_set_client = utf8mb4;
 
 use mimosa;
 
-INSERT INTO user(user_id, name, actevated) VALUES
+INSERT INTO user(name, actevated) VALUES
   (1001, 'alice', 'true'),
   (1002, 'bob', 'false');
 
@@ -15,8 +15,15 @@ INSERT INTO finding(finding_id, description, data_source, data_source_id, resour
   (1002, 'desc-1002', 'aws:access-analizer',  'access-analizer-0001', 'arn:aws:s3:::example-bucket',          1001, 99.05, 0.99,  '{"data":{"key":"value"}}'),
   (1003, 'desc-1003', 'aws:iam-checker',      'iam-checker-0001',     'arn:aws:iam::123456789012:user/alice', 1001, 100.00, 1.00, '{"data":{"key":"value"}}');
 
+INSERT INTO finding_tag(finding_tag_id, finding_id, tag_key, tag_value) VALUES
+  (1001, 1001, "key", "value");
+
 INSERT INTO resource(resource_id, resource_name, project_id) VALUES
   (1001, 'arn:aws:s3:::example-bucket',          1001),
   (1002, 'arn:aws:iam::123456789012:user/alice', 1001);
+
+INSERT INTO resource_tag(resource_tag_id, resource_id, tag_key, tag_value) VALUES
+  (1001, 1001, 'key1', "value"),
+  (1002, 1001, 'key2', "value");
 
 commit;
