48433fbd221b313be8c335091e687847cac5724d
gassara-kys <ogasawara_kiyoshi@cyberagent.co.jp>
2020-06-16 21:35:11 +0900

    add project_id for tag entities

--- local/db/02_dml_mimosa.sql
+++ local/db/02_dml_mimosa.sql
@@ -38,15 +38,15 @@ INSERT INTO finding(finding_id, description, data_source, data_source_id, resour
   (1002, 'desc-1002', 'aws:access-analizer', 'access-analizer-0001', 'arn:aws:s3:::example-bucket',          1001, 99.05, 0.99,  '{"data":{"key":"value"}}'),
   (1003, 'desc-1003', 'aws:iam-checker',     'iam-checker-0001',     'arn:aws:iam::123456789012:user/alice', 1001, 100.00, 1.00, '{"data":{"key":"value"}}');
 
-INSERT INTO finding_tag(finding_tag_id, finding_id, tag_key, tag_value) VALUES
-  (1001, 1001, "key", "value");
+INSERT INTO finding_tag(finding_tag_id, finding_id, project_id, tag_key, tag_value) VALUES
+  (1001, 1001, 1001, "key", "value");
 
 INSERT INTO resource(resource_id, resource_name, project_id) VALUES
   (1001, 'arn:aws:s3:::example-bucket',          1001),
   (1002, 'arn:aws:iam::123456789012:user/alice', 1001);
 
-INSERT INTO resource_tag(resource_tag_id, resource_id, tag_key, tag_value) VALUES
-  (1001, 1001, 'key1', "value"),
-  (1002, 1001, 'key2', "value");
+INSERT INTO resource_tag(resource_tag_id, resource_id, project_id, tag_key, tag_value) VALUES
+  (1001, 1001, 1001, 'key1', "value"),
+  (1002, 1001, 1001, 'key2', "value");
 
 commit;
