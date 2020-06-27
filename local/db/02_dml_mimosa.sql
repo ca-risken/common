11fc4362fafdf67df0dc581a1b35d5a214371290
gassara-kys <ogasawara_kiyoshi@cyberagent.co.jp>
2020-06-28 02:40:33 +0900

    add sqs

--- local/db/02_dml_mimosa.sql
+++ local/db/02_dml_mimosa.sql
@@ -59,7 +59,9 @@ INSERT INTO aws(aws_id, name, project_id, aws_account_id) VALUES
 INSERT INTO aws_data_source(aws_data_source_id, data_source, max_score) VALUES
   (1001, 'aws:guard-duty', 10.0),
   (1002, 'aws:access-analyzer', 1.0),
-  (1003, 'aws:iam-admin', 1.0);
+  (1003, 'aws:prowler', 1.0),
+  (1004, 'aws:iam-activity', 1.0),
+  (1005, 'aws:iam-admin', 1.0);
 
 INSERT INTO aws_rel_data_source(aws_id, aws_data_source_id, project_id, assume_role_arn, external_id) VALUES
   (1001, 1001, 1001, 'arn:aws:iam::123456789012:role/role-name', ''),
