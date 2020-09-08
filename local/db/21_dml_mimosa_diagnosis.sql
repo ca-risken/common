6362f01f42fe87fb5d41328ff4d23c8f3ddf38a0
iiiidaaa <iida_kazunori@cyberagent.co.jp>
2020-09-08 13:09:13 +0900

    add diagnosis,osint dml

---
+++ local/db/21_dml_mimosa_diagnosis.sql
@@ -0,0 +1,19 @@
+set character_set_connection = utf8mb4;
+set character_set_client = utf8mb4;
+
+use mimosa;
+
+INSERT INTO diagnosis(diagnosis_id, project_id, name) VALUES
+  (1, 1001, 'project-a-diagnosis'),
+  (2, 1001, 'project-a-diagnosis_2'),
+  (3, 1001, 'project-a-diagnosis_3');
+
+INSERT INTO diagnosis_data_source(diagnosis_data_source_id, name, description, max_score) VALUES
+  (1, 'intrigue', 'intrigue', 10.0);
+
+INSERT INTO rel_diagnosis_data_source(rel_diagnosis_data_source_id, diagnosis_data_source_id, diagnosis_id, project_id, record_id, jira_id, jira_key) VALUES
+  (1, 1, 1, 1001, '1353', '', ''),
+  (2, 1, 1, 1001, '', '10241', ''),
+  (3, 1, 1, 1001, '', '', 'OC202001');
+
+commit;
