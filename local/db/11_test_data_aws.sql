05fedb39110b38039adc0535a3e26166e4692e84
gassara-kys <ogasawara_kiyoshi@cyberagent.co.jp>
2020-10-07 22:12:26 +0900

    delete dupulicated data

--- local/db/11_test_data_aws.sql
+++ local/db/11_test_data_aws.sql
@@ -8,9 +8,9 @@ INSERT INTO aws(aws_id, name, project_id, aws_account_id) VALUES
   (1001, 'account-a', 1001, '123456789012');
 
 INSERT INTO aws_rel_data_source(aws_id, aws_data_source_id, project_id, assume_role_arn, external_id, status, status_detail, scan_at) VALUES
-  (1001, 1001, 1001, 'arn:aws:iam::123456789012:role/role-name', '', 'configured', '', null),
-  (1001, 1002, 1001, 'arn:aws:iam::123456789012:role/role-name', '', 'configured', '', null),
-  (1001, 1003, 1001, 'arn:aws:iam::123456789012:role/role-name', '', 'configured', '', null);
+  (1001, 1001, 1001, 'arn:aws:iam::123456789012:role/role-name', '', 'CONFIGURED', '', null),
+  (1001, 1002, 1001, 'arn:aws:iam::123456789012:role/role-name', '', 'CONFIGURED', '', null),
+  (1001, 1003, 1001, 'arn:aws:iam::123456789012:role/role-name', '', 'CONFIGURED', '', null);
 
 
 commit;
