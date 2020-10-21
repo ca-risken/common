02139c94d07c968cdc5540de89df30ad9127f134
iiiidaaa <iida_kazunori@cyberagent.co.jp>
2020-10-21 14:52:46 +0900

    fix subapp field

--- local/db/12_test_data_osint.sql
+++ local/db/12_test_data_osint.sql
@@ -5,11 +5,11 @@ use mimosa;
 
 -- OSINT ------------------------------------------------
 INSERT INTO osint(osint_id, project_id, name) VALUES
-  (1, 1001, 'project-a-osint'),
-  (2, 1001, 'project-a-osint_2');
+  (1001, 1001, 'project-a-osint'),
+  (1002, 1001, 'project-a-osint_2');
 
-INSERT INTO osint_result(osint_result_id, osint_data_source_id, osint_id, project_id, resource_type, resource_name) VALUES
-  (1, 1, 1, 1001, 'Domain', 'security-hub.jp'),
-  (2, 1, 1, 1001, 'DnsRecord', 'security-hub.jp');
+INSERT INTO rel_osint_data_source(rel_osint_data_source_id, osint_data_source_id, osint_id, project_id, resource_type, resource_name, status) VALUES
+  (1001, 1001, 1001, 1001, 'Domain', 'security-hub.jp', 'CONFIGURED'),
+  (1002, 1001, 1001, 1001, 'DnsRecord', 'security-hub.jp', 'CONFIGURED');
 
 commit;
--- local/db/13_test_data_diagnosis.sql
+++ local/db/13_test_data_diagnosis.sql
@@ -4,7 +4,7 @@ set character_set_client = utf8mb4;
 use mimosa;
 
 INSERT INTO jira_setting(jira_setting_id, name, diagnosis_data_source_id, project_id, identity_field, identity_value, jira_id, jira_key, status, status_detail, scan_at) VALUES
-  (1001, "test-a", 1001, 1001, 'customfield_10035', '1393', '', '', 'CONFIGURED', '', null),
+  (1001, "test-a", 1001, 1001, '10035', '1393', '', '', 'CONFIGURED', '', null),
   (1002, "test-b", 1001, 1001, '', '', '10241', '', 'CONFIGURED', '', null),
   (1003, "test-c", 1001, 1001, '', '', '', 'OC202001', 'CONFIGURED', '', null);
 
