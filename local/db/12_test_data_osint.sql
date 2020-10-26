ecaaf2785929a0d9b1deb59965faacafb83f80a6
iiiidaaa <iida_kazunori@cyberagent.co.jp>
2020-10-27 01:32:44 +0900

    fix osint table,column,sqs queue
    OSINTの修正に伴いテーブル構成を変更しました。
    またOSINTツールの実行時間を勘案し、可視性タイムアウトを900秒に延長しました

--- local/db/12_test_data_osint.sql
+++ local/db/12_test_data_osint.sql
@@ -4,12 +4,21 @@ set character_set_client = utf8mb4;
 use mimosa;
 
 -- OSINT ------------------------------------------------
-INSERT INTO osint(osint_id, project_id, name) VALUES
-  (1001, 1001, 'project-a-osint'),
-  (1002, 1001, 'project-a-osint_2');
+INSERT INTO osint(osint_id, project_id, resource_type, resource_name) VALUES
+  (1001, 1001, 'Domain', 'security-hub.jp'),
+  (1002, 1001, 'Domain', 'cyberagent.co.jp');
 
-INSERT INTO rel_osint_data_source(rel_osint_data_source_id, osint_data_source_id, osint_id, project_id, resource_type, resource_name, status) VALUES
-  (1001, 1001, 1001, 1001, 'Domain', 'security-hub.jp', 'CONFIGURED'),
-  (1002, 1001, 1001, 1001, 'DnsRecord', 'security-hub.jp', 'CONFIGURED');
+INSERT INTO rel_osint_data_source(rel_osint_data_source_id, osint_data_source_id, osint_id, project_id, status) VALUES
+  (1001, 1001, 1001, 1001, 'CONFIGURED'),
+  (1002, 1001, 1001, 1001, 'CONFIGURED');
+
+INSERT INTO rel_osint_detect_word(rel_osint_detect_word_id,rel_osint_data_source_id,osint_detect_word_id, project_id) VALUES
+  (1001, 1001, 1001, 1001),
+  (1002, 1001, 1002, 1001);
+
+INSERT INTO osint_detect_word(osint_detect_word_id,word,project_id) VALUES
+  (1001, 'jenkins', 1001),
+  (1002, 'stg', 1001),
+  (1003, 'admin', 1001);
 
 commit;
