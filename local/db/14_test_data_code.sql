b13b3edb65ba691dc462be59ead5e16b0cf71e91
gassara-kys <ogasawara_kiyoshi@cyberagent.co.jp>
2020-11-18 16:58:15 +0900

    fix gitleaks

---
+++ local/db/14_test_data_code.sql
@@ -0,0 +1,9 @@
+set character_set_connection = utf8mb4;
+set character_set_client = utf8mb4;
+
+use mimosa;
+
+INSERT INTO gitleaks(gitleaks_id, code_data_source_id, name, project_id, type, target_resource, repository_pattern, github_user, personal_access_token, gitleaks_config, status, status_detail, scan_at) VALUES
+  (1001, 1001, "gitleaksk-name", 1001, 'USER', 'gitleakstest', 'gronit', '', '', '', 'CONFIGURED', '', null);
+
+commit;
