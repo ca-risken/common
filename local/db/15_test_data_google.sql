841b1482a243873f9eeb1d25e741286b72123fa1
gassara-kys <ogasawara_kiyoshi@cyberagent.co.jp>
2020-12-22 17:40:29 +0900

    add google entity

---
+++ local/db/15_test_data_google.sql
@@ -0,0 +1,9 @@
+set character_set_connection = utf8mb4;
+set character_set_client = utf8mb4;
+
+use mimosa;
+
+INSERT INTO google_gcp(gcp_id, google_data_source_id, name, project_id, gcp_organization_id, gcp_project_id, scan_at) VALUES
+  (1001, 1001, "name", 1001, 'my-org', 'my-project', null);
+
+commit;
