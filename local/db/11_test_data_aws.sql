5b787fa253bcf4e7e3c689b57d7e509eaa23915d
iiiidaaa <oeriver@gmail.com>
2021-01-19 19:42:27 +0900

    add-aws-portscan

--- local/db/11_test_data_aws.sql
+++ local/db/11_test_data_aws.sql
@@ -11,5 +11,6 @@ INSERT INTO aws_rel_data_source(aws_id, aws_data_source_id, project_id, assume_r
   (1001, 1001, 1001, 'arn:aws:iam::123456789012:role/role-name', '', 'CONFIGURED', '', null),
   (1001, 1002, 1001, 'arn:aws:iam::123456789012:role/role-name', '', 'CONFIGURED', '', null),
   (1001, 1003, 1001, 'arn:aws:iam::123456789012:role/role-name', '', 'CONFIGURED', '', null),
-  (1001, 1004, 1001, 'arn:aws:iam::123456789012:role/role-name', '', 'CONFIGURED', '', null);
+  (1001, 1004, 1001, 'arn:aws:iam::123456789012:role/role-name', '', 'CONFIGURED', '', null),
+  (1001, 1005, 1001, 'arn:aws:iam::123456789012:role/role-name', '', 'CONFIGURED', '', null);
 commit;
