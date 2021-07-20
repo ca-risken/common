766f4475bfd181c831a783461e1bb562cc2a9b47
mmmknt <mmmknt@mmmknt.dev>
2021-07-20 16:16:37 +0900

    Fix http tracing middleware interface

--- pkg/xray/http.go
+++ pkg/xray/http.go
@@ -7,24 +7,30 @@ import (
 	"github.com/aws/aws-xray-sdk-go/xray"
 )
 
-func IgnoreHealthCheckTracingHandler(segmentName, healthzPath string, next http.Handler) http.Handler {
-	xrh := xray.Handler(xray.NewFixedSegmentNamer(segmentName), next)
-	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		if r.URL.Path != healthzPath {
-			xrh.ServeHTTP(w, r)
-		} else {
-			next.ServeHTTP(w, r)
+func IgnoreHealthCheckTracingMiddleware(segmentName, healthzPath string) func(http.Handler) http.Handler {
+	return func(next http.Handler) http.Handler {
+		xrh := xray.Handler(xray.NewFixedSegmentNamer(segmentName), next)
+		fn := func(w http.ResponseWriter, r *http.Request) {
+			if r.URL.Path != healthzPath {
+				xrh.ServeHTTP(w, r)
+			} else {
+				next.ServeHTTP(w, r)
+			}
 		}
-	})
+		return http.HandlerFunc(fn)
+	}
 }
 
-func AnnotateEnvTracingHandler(envName string, next http.Handler) http.Handler {
-	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		if err := xray.AddAnnotation(r.Context(), "env", envName); err != nil {
-			// TODO logger
-			// appLogger.Warnf("failed to annotate environment to x-ray: %+v", err)
-			fmt.Printf("failed to annotate environment to x-ray: %+v", err)
+func AnnotateEnvTracingMiddleware(envName string) func(http.Handler) http.Handler {
+	return func(next http.Handler) http.Handler {
+		fn := func(w http.ResponseWriter, r *http.Request) {
+			if err := xray.AddAnnotation(r.Context(), "env", envName); err != nil {
+				// TODO logger
+				// appLogger.Warnf("failed to annotate environment to x-ray: %+v", err)
+				fmt.Printf("failed to annotate environment to x-ray: %+v", err)
+			}
+			next.ServeHTTP(w, r)
 		}
-		next.ServeHTTP(w, r)
-	})
+		return http.HandlerFunc(fn)
+	}
 }
