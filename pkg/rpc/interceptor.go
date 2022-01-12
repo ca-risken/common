fc38f2c7a5dbad884a1e464d9cc2f750c9394f67
gassara-kys <ogasawara_kiyoshi@cybernagent>
2022-01-12 17:24:51 +0900

    fix: logger

--- pkg/rpc/interceptor.go
+++ pkg/rpc/interceptor.go
@@ -4,25 +4,27 @@ import (
 	"context"
 	"time"
 
-	"github.com/sirupsen/logrus"
+	"github.com/ca-risken/common/pkg/logging"
 	"google.golang.org/grpc"
 	"google.golang.org/grpc/status"
 )
 
-func LoggingUnaryServerInterceptor(logger *logrus.Logger) grpc.UnaryServerInterceptor {
+func LoggingUnaryServerInterceptor(logger logging.Logger) grpc.UnaryServerInterceptor {
 	return func(ctx context.Context, req interface{}, info *grpc.UnaryServerInfo, handler grpc.UnaryHandler) (interface{}, error) {
 		start := time.Now()
 		res, err := handler(ctx, req)
 		method := info.FullMethod
 		code := status.Code(err)
 		elapsed := time.Since(start).Seconds()
-		entry := logger.WithField("method", method).
-			WithField("code", code.String()).
-			WithField("elapsed", elapsed)
-		if err != nil {
-			entry = entry.WithField("error", shorten(err.Error()))
+		items := map[string]interface{}{
+			"method":  method,
+			"code":    code.String(),
+			"elapsed": elapsed,
 		}
-		entry.Infof("receive from client")
+		if err != nil {
+			items["error"] = shorten(err.Error())
+		}
+		logger.WithItems(logging.InfoLevel, items, "receive from client")
 		return res, err
 	}
 }
