f4264f237b65ede6b0a34c2b5666e4605ec40966
gassara-kys <ogasawara_kiyoshi@cybernagent>
2022-04-01 12:56:12 +0900

    fix: finalizer use default template

--- pkg/sqs/grpc.go
+++ pkg/sqs/grpc.go
@@ -19,7 +19,6 @@ func newFindingClient(svcAddr string) finding.FindingServiceClient {
 }
 
 func getGRPCConn(ctx context.Context, addr string) (*grpc.ClientConn, error) {
-	// gRPCクライアントの呼び出し回数が非常に多くトレーシング情報の送信がエラーになるため、トレースは無効にしておく
 	ctx, cancel := context.WithTimeout(ctx, 3*time.Second)
 	defer cancel()
 	conn, err := grpc.DialContext(ctx, addr, grpc.WithTransportCredentials(insecure.NewCredentials()))
