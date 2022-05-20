43925ced3ef1da037caab15ca20b02481e8ea895
mmmknt <mmmknt@mmmknt.dev>
2022-05-20 18:49:37 +0900

    Integrate log and trace

--- pkg/grpc_client/finding.go
+++ pkg/grpc_client/finding.go
@@ -8,14 +8,14 @@ import (
 
 // PutFindingBatch call PutFindingBatch API at each request limit
 func PutFindingBatch(ctx context.Context, client finding.FindingServiceClient, projectID uint32, params []*finding.FindingBatchForUpsert) error {
-	appLogger.Infof("Putting findings(%d)...", len(params))
+	appLogger.Infof(ctx, "Putting findings(%d)...", len(params))
 	for idx := 0; idx < len(params); idx = idx + finding.PutFindingBatchMaxLength {
 		lastIdx := idx + finding.PutFindingBatchMaxLength
 		if lastIdx > len(params) {
 			lastIdx = len(params)
 		}
 		// request per API limits
-		appLogger.Debugf("Call PutFindingBatch API, (%d ~ %d / %d)", idx+1, lastIdx, len(params))
+		appLogger.Debugf(ctx, "Call PutFindingBatch API, (%d ~ %d / %d)", idx+1, lastIdx, len(params))
 		req := &finding.PutFindingBatchRequest{ProjectId: projectID, Finding: params[idx:lastIdx]}
 		if _, err := client.PutFindingBatch(ctx, req); err != nil {
 			return err
@@ -26,14 +26,14 @@ func PutFindingBatch(ctx context.Context, client finding.FindingServiceClient, p
 
 // PutResourceBatch call PutResourceBatch API at each request limit
 func PutResourceBatch(ctx context.Context, client finding.FindingServiceClient, projectID uint32, params []*finding.ResourceBatchForUpsert) error {
-	appLogger.Infof("Putting resources(%d)...", len(params))
+	appLogger.Infof(ctx, "Putting resources(%d)...", len(params))
 	for idx := 0; idx < len(params); idx = idx + finding.PutResourceBatchMaxLength {
 		lastIdx := idx + finding.PutResourceBatchMaxLength
 		if lastIdx > len(params) {
 			lastIdx = len(params)
 		}
 		// request per API limits
-		appLogger.Debugf("Call PutResourceBatch API, (%d ~ %d / %d)", idx+1, lastIdx, len(params))
+		appLogger.Debugf(ctx, "Call PutResourceBatch API, (%d ~ %d / %d)", idx+1, lastIdx, len(params))
 		req := &finding.PutResourceBatchRequest{ProjectId: projectID, Resource: params[idx:lastIdx]}
 		if _, err := client.PutResourceBatch(ctx, req); err != nil {
 			return err
