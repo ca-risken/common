5ea0b277451bb0953edf572a74bbc523ae8409f7
iiiidaaa <iida_kazunori@cyberagent.co.jp>
2022-11-29 14:56:46 +0900

    fix create table

--- middleware/db/01_ddl_mimosa.sql
+++ middleware/db/01_ddl_mimosa.sql
@@ -12,7 +12,7 @@ CREATE TABLE user (
   updated_at DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
   PRIMARY KEY(user_id),
   UNIQUE KEY uidx_sub (sub),
-  UNIQUE KEY uidx_user_idp_key (user_idp_key),
+  UNIQUE KEY uidx_user_idp_key (user_idp_key)
 ) ENGINE = InnoDB DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_bin AUTO_INCREMENT = 1001;
 
 CREATE TABLE user_role (
