b71c8e47cd1beb8e2df90d16353dee7df43a89a4
gassara-kys <ogasawara_kiyoshi@cybernagent>
2023-07-20 12:21:59 +0900

    add: sampling rate option

--- pkg/tracer/tracer.go
+++ pkg/tracer/tracer.go
@@ -5,16 +5,26 @@ import (
 )
 
 type Config struct {
-	ServiceName string
-	Environment string
-	Debug       bool
+	ServiceName  string
+	Environment  string
+	Debug        bool
+	SamplingRate *float64 // Set a sampling rate value in the range of 0.0000 to 1.0000. (e.g.) 0.3 = 30%
 }
 
 func Start(c *Config) {
+	rate := 1.0000 // 100%
+	if c.SamplingRate != nil {
+		rate = *c.SamplingRate
+	}
 	tracerOpts := []ddtracer.StartOption{
 		ddtracer.WithEnv(c.Environment),
 		ddtracer.WithService(c.ServiceName),
 		ddtracer.WithDebugMode(c.Debug),
+		ddtracer.WithSamplingRules(
+			[]ddtracer.SamplingRule{
+				ddtracer.ServiceRule(c.ServiceName, rate),
+			},
+		),
 	}
 	ddtracer.Start(tracerOpts...)
 }
