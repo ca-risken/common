abf64c39e6330269a19a892b22783ffaab740c01
Senk <44151180+senk8@users.noreply.github.com>
2025-12-09 15:04:00 +0900

    remove unnecessary newline at the end of 01_ddl_mimosa.sql

--- middleware/db/01_ddl_mimosa.sql
+++ middleware/db/01_ddl_mimosa.sql
@@ -763,4 +763,3 @@ CREATE TABLE rel_azure_data_source (
   updated_at DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
   PRIMARY KEY(azure_id, azure_data_source_id)
 ) ENGINE = InnoDB DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_bin;
-
